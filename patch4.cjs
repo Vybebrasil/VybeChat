3d963a6f0990017efe96b3de5eeb435f82d61b7c
Vybebrasil <gestaovybe@gmail.com>
2026-08-24 13:56:01 -0300

    chore: remove patch files

--- patch4.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('client/src/pages/CloudflareHome.tsx', 'utf8');
-
-c = c.replace('<div className="vybe-message-list">', '<div className="vybe-message-list">\n<AnimatePresence initial={false}>');
-c = c.replace('</Fragment>\n                    ))}', '</Fragment>\n                    )}\n</AnimatePresence>');
-
-fs.writeFileSync('client/src/pages/CloudflareHome.tsx', c);
-console.log('done4');
